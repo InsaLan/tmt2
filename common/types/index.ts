713fa23436b79111598844267d4cc13251c3cd3c
pixup1 <pixup1@yahoo.com>
2025-02-03 10:16:11 +0100

    Improve frontend error handling

--- common/types/index.ts
+++ common/types/index.ts
@@ -11,6 +11,7 @@ export * from './matchMap';
 export * from './player';
 export * from './preset';
 export * from './stats';
+export * from './status';
 export * from './stuff';
 export * from './team';
 export * from './webSocket';
