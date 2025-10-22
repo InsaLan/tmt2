00b728f60011180baf0071dd54ef42e17d925b11
pixup1 <pixup1@yahoo.com>
2025-10-23 01:51:18 +0200

    Add stats management page

--- frontend/src/pages/stats.tsx
+++ frontend/src/pages/stats.tsx
@@ -15,6 +15,7 @@ import {
 } from '../utils/playerStatsUtils';
 import { StatsTable } from '../components/StatsTable';
 import { CopyableText } from '../components/CopyableText';
+import { SvgDelete, SvgDownloadArchive, SvgUploadCloud } from '../assets/Icons';
 
 export const MatchesStatsPage = () => {
 	const fetcher = createFetcher();
@@ -484,3 +485,36 @@ export const PlayerStatsPage = () => {
 		</>
 	);
 };
+
+export const DataManagementStatsPage = () => {
+	return (
+		<>
+			<StatsNavBar />
+			<Card>
+				<div class="items-center justify-center flex gap-4">
+					<button
+						class="btn"
+						onClick={() => {}}
+					>
+						<SvgDownloadArchive/>
+						{t('Download database')}
+					</button>
+					<button
+						class="btn text-error"
+						onClick={() => {}}
+					>
+						<SvgUploadCloud/>
+						{t('Import database file')}
+					</button>
+					<button
+						class="btn text-error"
+						onClick={() => {}}
+					>
+						<SvgDelete/>
+						{t('Empty database')}
+					</button>
+				</div>
+			</Card>
+		</>
+	)
+}
